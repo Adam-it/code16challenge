ddb5d272bf588d73ca83eadeea5385b3e05830bb
Adam <adam.wojcik.it@gmail.com>
2021-02-13 01:19:29 +0100

    added code

---
+++ DeveloperHelper/DeveloperHelper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Configuration;
+using System.Threading;
+using Windows.UI.Notifications;
+
+namespace DeveloperHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);               
+                toastXml.GetElementsByTagName("text")[0].AppendChild(toastXml.CreateTextNode(ConfigurationManager.AppSettings[$"message{new Random().Next(1, 11)}"]));
+                ToastNotificationManager.CreateToastNotifier(nameof(DeveloperHelper)).Show(new ToastNotification(toastXml));
+                Thread.Sleep(Convert.ToInt32(ConfigurationManager.AppSettings["sleep"]));
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# code16challenge
+## Developer helper
+---
+### 🎯 Aim 
+The aim was to develop some simple app in 16 lines of code to suppor the [#code16challenge](https://www.facebook.com/hashtag/code16challenge) facebook event
+
+---
+### 🙄 What was done
+I added a console app which every couple of minutes presents a random push notification message. The sleep time and the messages may be changed in the App.config file
+
+---
+### 🚀 How to run
+1. clone
+2. I guess you will need to restore nugets 
+![](./Images/Nugets.png)
+3. I suppose you will need to fix reverences to windows dll's
+![](./Images/windowsDlls.png)
+4. run 👍
+
+---
+### 📸 Result
+some notifications that show from time to time
+![](./Images/Notifications.png)
+
+notifications summary
+![](./Images/NotificationSummary.png)
